ba9f539dd015d254d5c0954588499014193a5a9d
zhenghz <zhenghz@sunline.cn>
2017-07-15 11:27:30 +0800

    eclipse plugin java code transfer

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # codeTrans
 a eclipse  plugin which use to change the project and javacode charset 
+
+## Features
+
+ 1. Change project and java file from GBk to 
+ 2. Change project and java file from UTF-8 to GBK
+ 
+ ## Donate
+ 
+If you find this software useful and would like to support it, you can do so simply by scanning my Alipay two-dimension code and donating whatever you like.
+
+![My code](code.png)
+ 
---
+++ src/codetrans/popup/actions/Gbk2Utf8.java
@@ -0,0 +1,98 @@
+package codetrans.popup.actions;
+
+import java.io.File;
+import java.io.FilenameFilter;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.IPackageFragmentRoot;
+import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jdt.core.JavaModelException;
+import org.eclipse.jdt.internal.core.JarPackageFragmentRoot;
+import org.eclipse.jdt.internal.core.JavaProject;
+import org.eclipse.jdt.internal.core.PackageFragmentRoot;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.IActionDelegate;
+import org.eclipse.ui.IObjectActionDelegate;
+import org.eclipse.ui.IWorkbenchPart;
+
+import codetrans.test.FileCharsetConverter;
+
+public class Gbk2Utf8 implements IObjectActionDelegate {
+
+	private Shell shell;
+	private ISelection selection;
+
+	/**
+	 * Constructor for Action1.
+	 */
+	public Gbk2Utf8() {
+		super();
+	}
+
+	/**
+	 * @see IObjectActionDelegate#setActivePart(IAction, IWorkbenchPart)
+	 */
+	public void setActivePart(IAction action, IWorkbenchPart targetPart) {
+		shell = targetPart.getSite().getShell();
+	}
+
+	/**
+	 * @see IActionDelegate#run(IAction)
+	 */
+	public void run(IAction action) {
+		if(selection instanceof IStructuredSelection){
+			IStructuredSelection structuredSelection = (IStructuredSelection) selection;
+			Object object = structuredSelection.getFirstElement();
+			IJavaProject javaProject = null;
+			File parentFile = null;
+			if(object instanceof IProject){
+				IProject iProject =  (IProject) object;
+				parentFile = iProject.getLocation().toFile();
+				javaProject = JavaCore.create(iProject); 
+			}else if(object instanceof IJavaProject){
+				javaProject = (IJavaProject) object;
+				parentFile = javaProject.getProject().getLocation().toFile();
+			}
+			
+			try {
+				javaProject.getProject().refreshLocal(0, null);
+				IPackageFragmentRoot[] fragmentRoots = 	javaProject.getPackageFragmentRoots();
+				if(fragmentRoots==null)return;
+				for (IPackageFragmentRoot iPackageFragmentRoot : fragmentRoots) {
+					if((iPackageFragmentRoot instanceof JarPackageFragmentRoot))continue;
+					File file =   iPackageFragmentRoot.getPath().toFile();
+					File f  = new File(parentFile, file.getName());
+					FileCharsetConverter.convert(f,"GBK","UTF-8",  new FilenameFilter() {  
+				        @Override  
+				        public boolean accept(File dir, String name) {  
+				        	if(dir.isDirectory())return true;
+				            return name.endsWith("java");  
+				        }  
+				    });
+				}
+				javaProject.getProject().setDefaultCharset("UTF-8",null);
+				javaProject.getProject().refreshLocal(0, null);
+			} catch (JavaModelException e) {
+				e.printStackTrace();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			
+		}
+		
+
+	}
+
+	/**
+	 * @see IActionDelegate#selectionChanged(IAction, ISelection)
+	 */
+	public void selectionChanged(IAction action, ISelection selection) {
+		this.selection = selection;
+	}
+
+}
